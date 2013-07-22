ea028bbfaa60d72e129ff63ffaef46028481827b
Mureithi <kelvinmwas@gmail.com>
2013-07-22 18:41:36 +0300

    District

--- application/views/district/district_dash.php
+++ application/views/district/district_dash.php
@@ -1,298 +1,253 @@
-<SCRIPT LANGUAGE="Javascript" SRC="<?php echo base_url(); ?>Scripts/FusionCharts/FusionCharts.js"></SCRIPT>
+<SCRIPT LANGUAGE="Javascript" SRC="<?php echo base_url();?>Scripts/FusionCharts/FusionCharts.js"></SCRIPT>
+<script type="text/javascript" language="javascript" src="<?php echo base_url(); ?>Scripts/jquery.dataTables.js"></script>
+    <style type="text/css" title="currentStyle">
+      
+      @import "<?php echo base_url(); ?>DataTables-1.9.3 /media/css/jquery.dataTables.css";
+    </style>
+ 
+    <style>
 
-<script>
-		$(document).ready(function(){
-	        //$('.accordion').accordion({defaultOpen: ''});
-         //custom animation for open/close
-    $.fn.slideFadeToggle = function(speed, easing, callback) {
-        return this.animate({opacity: 'toggle', height: 'toggle'}, speed, easing, callback);
-    };
-    $('.accordion').accordion({
-        defaultOpen: 'section1',
-        cookieName: 'nav',
-        speed: 'medium',
-        animateOpen: function (elem, opts) { //replace the standard slideUp with custom function
-            elem.next().slideFadeToggle(opts.speed);
-        },
-        animateClose: function (elem, opts) { //replace the standard slideDown with custom function
-            elem.next().slideFadeToggle(opts.speed);
-        }
-    });
-    //default call
-    var url = "<?php echo base_url().'report_management/get_stock_status_ajax'?>"
-		var div=".stockstatuschart";
-		ajax_request (url,div);
+      .warning2 {
+  background: #FEFFC8 url('<?php echo base_url()?>Images/excel-icon.jpg') 20px 50% no-repeat;
+  border: 1px solid #F1AA2D;
+  }
+    </style>
 
-		var url = "<?php echo base_url().'report_management/get_costofexpiries_chart_ajax'?>"
-		var div=".cost_expirieschart";
-		ajax_request (url,div);
-
-		var url = "<?php echo base_url().'report_management/get_costoforders_chart_ajax'?>";
-		var div=".cost_orderschart";
-		ajax_request (url,div);
-
-		var url = "<?php echo base_url().'report_management/get_consumption_trends_ajax' ?>	";
-		var div=".consumptionchart";
-		ajax_request (url,div);
-
-		var url = "<?php echo base_url().'report_management/get_leadtime_chart_ajax'?>";
-		var div=".lead_timechart";
+    <script type="text/javascript" charset="utf-8">
+      
+      $(document).ready(function() {
+      	//ajax reuest for the graphs
+        var url = "<?php echo base_url()."report_management/get_stock_status_ajax/ajax-request_county"?>";			
+		var div="#chart3";
+		
+		ajax_request(url,div);
+		
+		var url = "<?php echo base_url().'report_management/get_costofexpiries_chart_ajax/county/'?>"
+		var div="#chart2";
+		
 		ajax_request (url,div);
 		
 		
-       $('#facilities').change(function() {
-       	var div=".stockstatuschart";
-		var url = "<?php echo base_url().'report_management/get_district_facility_stock_/'?>";	
-		url=url+"bar2d_facility/"+$(this).val();
-		ajax_request(url,div);
+		$('#desc').change(function() {
+         var div="#chart3";
+		var url = "<?php echo base_url()."report_management/get_stock_status_ajax/ajax-request_county"?>";		
+		url=url+"/"+$(this).val();
+		
+		if($(this).val() =='default') {
+			return;	
+		}else{
+		ajax_request (url,div)
+		}
+		
+		
 		});
-
-        $('#desc').change(function() {
-        var div=".stockstatuschart";
-		var url = "<?php echo base_url().'report_management/get_district_facility_stock_/'?>";	
-		url=url+"bar2d_drug/"+$(this).val();
-		ajax_request(url,div);
-		});
-
+		
+		
 		function ajax_request (url,div){
-		var url =url;
-		var loading_icon="<?php echo base_url().'Images/loader.gif' ?>";
-		$.ajax({
-		type: "POST",
-		url: url,
-		beforeSend: function() {
-		$(div).html("");
+	var url =url;
+	var loading_icon="<?php echo base_url().'Images/loader.gif' ?>";
+	 $.ajax({
+          type: "POST",
+          url: url,
+          beforeSend: function() {
+            $(div).html("");
+            
+             $(div).html("<img style='margin-top:-10%;' src="+loading_icon+">");
+            
+          },
+          success: function(msg) {
+          $(div).html("");
+            $(div).html(msg);           
+          }
+        }); 
+}
+    
+  
+     
+    var chart = new FusionCharts("<?php echo base_url()."scripts/FusionCharts/Bar2D.swf"?>","ChartId", "100%", "80%", "0", "1" );
+    var url = '<?php echo base_url()."report_management/expired_commodities_chart"?>'; 
+    chart.setDataURL(url);
+    chart.render("chart1");
 
-		$(div).html("<img style='margin-left:20%;' src="+loading_icon+">");
+    
 
-		},
-		success: function(msg) {
-		$(div).html("");
-		$(div).html(msg);
-		}
-		});
-		}
+  
 
-		});
-</script>
+     var chart = new FusionCharts("<?php echo base_url()."scripts/FusionCharts/Line.swf"?>", "ChartId3", "100%", "70%", "0", "0");
+    var url = '<?php echo base_url()."report_management/orders_chart"?>'; 
+    chart.setDataURL(url);
+    chart.render("chart4");
+
+      var chart = new FusionCharts("<?php echo base_url()."scripts/FusionCharts/Line.swf"?>", "ChartId4", "100%", "80%", "0", "0");
+    var url = '<?php echo base_url()."report_management/cost_of_ordered_commodities_chart"?>'; 
+    chart.setDataURL(url);
+    chart.render("chart5");
+
+     var chart = new FusionCharts("<?php echo base_url()."scripts/FusionWidgets/AngularGauge.swf"?>", "ChartId5", "100%", "80%", "0", "0");
+    var url = '<?php echo base_url()."report_management/cummulative_fill_rate_chart"?>'; 
+    chart.setDataURL(url);
+    chart.render("chart6");
 
 
-<style>
-	.dash_container {
-		width: 96%;
-		margin: auto;
-		margin-bottom: 5em;
-	}
-	
-	.notifications_container{
-		padding-left:5%;
-		width:95%;
+     var chart = new FusionCharts("<?php echo base_url()."scripts/FusionCharts/StackedColumn2D.swf"?>", "ChartId6", "100%", "80%", "0", "0");
+    var url = '<?php echo base_url()."report_management/district_drawing_rights_chart"?>'; 
+    chart.setDataURL(url);
+    chart.render("chart7");
+
+     var chart = new FusionCharts("<?php echo base_url()."scripts/FusionCharts/Pie3D.swf"?>", "ChartId7", "100%", "80%", "0", "0");
+    var url = '<?php echo base_url()."report_management/orders_placed_chart"?>'; 
+    chart.setDataURL(url);
+    chart.render("chart8");
+
+     var chart = new FusionCharts("<?php echo base_url()."scripts/FusionWidgets/HLinearGauge.swf"?>", "ChartId8", "100%", "20%", "0", "0");
+    var url = '<?php echo base_url()."report_management/lead_time_chart_county"?>'; 
+    chart.setDataURL(url);
+    chart.render("chart9");
+    
+    var chart = new FusionCharts("<?php echo base_url()."scripts/FusionCharts/StackedColumn2D.swf"?>", "ChartId9", "100%", "80%", "0", "0");
+    var url = '<?php echo base_url()."report_management/county_ordering_rate_chart"?>'; 
+    chart.setDataURL(url);
+    chart.render("chart10");
+    
+    var chart = new FusionCharts("<?php echo base_url()."scripts/FusionCharts/MSColumn2D.swf"?>", "ChartId10", "100%", "80%", "0", "0");
+    var url = '<?php echo base_url()."report_management/County_orders_placed_chart"?>'; 
+    chart.setDataURL(url);
+    chart.render("chart11");
+
+
+  });
+  </script>
+  <style>
+  .dash_container {
+		width: 100%;
+		height:700px;
+		margin-left: 1.1em;
 		
 	}
-	.statistics {
-		width: 29%;
-		height: 35em;
-		float: left;
-		margin-right: 1.1em;
-		margin-bottom: 1em;
-		background:#d9d9d9;
-		
-	}
-	.statistics h2, .stockstatus h2, .cost_expiries h2, .cost_orders h2, .consumption h2, .lead_time h2 {
-		background: #29527B; /* Old browsers */
-		color: #fff;
-		padding: 10px;
+    .chart_content{
+    margin:0 auto;
+    width:100%;
+    
+  }
+  .multiple_chart_content{
+    float:left;
+    width:33%; 
+    height:22em; 
+    padding:0.2em
+    background-color:#0E90D2;
+    
+    
+  }
+  .multiple_chart_content h2 {
+		background: #b6b6b6; /* Old browsers */
+		padding: 5px;
 		text-align: center;
 		margin: 0 0 0.625em 0;
-		-webkit-box-shadow:  0px 2px 6px 0.7px #000;
-        box-shadow:  0px 2px 6px 0.7px #000;
-        -moz-box-shadow:  0px 2px 6px 0.7px #000;
-	}
-	.stockstatus {
-
-		width: 70%;
-		height: 35em;
-		float: left;
-		margin-bottom: 1em;
-	}
-	.stockstatus_dropdown {
-
-		width: 100%;
-		height: auto;
-		display: inline-block;
-		padding-bottom: 0.5em;
-		padding-top: 0.5em;
-		margin-left:12em;
-	}
-	.drop {
-		background: transparent;
-		height: 2em;
-		border: 1px solid #ccc;
-		padding: 0.2em;
-	}
-	.stockstatuschart {
-
-		width: 100%;
-		display: inline-block;
-	}
-
-	.cost_expiries {
-		width: 49%;
-		height: 36em;
-		float: left;
-		margin-right: 1.1em;
-		margin-bottom: 0.5em;
+		border-right-style: inset;
 		
 	}
-	.cost_expirieschart {
-		width: 100%;
-		margin: auto;
+	.multiple_chart_content label{
+		font-size:12px;
 	}
-	.cost_orders {
-		width: 49%;
-		height: 36em;
-		float: left;
-		margin-bottom: 0.5em;
-		
-	}
-	.cost_orderschart {
-		width: 100%;
-	}
-	.consumption {
-		width: 49%;
-		height: 40em;
-		float: left;
-		margin-right: 1.1em;
-		
-	}
-	.consumptionchart {
-		width: 100%;
-	}
-	.lead_time {
-		width: 49%;
-		height: 55em;
-		float: left;
-		margin-right: 1.1em;
-		
-	}
-	.lead_timechart {
-		width: 100%;
-	}
-	
+  
 </style>
-
-
+  
 <div class="dash_container">
-
-<div class="statistics">
-	<h2 style="margin-bottom: 1.5em">Statistics & Notification</h2>
-	<div class="notifications_container">
-		 
-  			<div style="display: table-row;  ">
-    			<div style="display: table-cell;padding-bottom: 3em; ">
-      				<label style=" font-weight: 600">Total No of Orders Placed </label>
-            			<a class="badge success" href="#" >14</a>
+	
+	<div class="multiple_chart_content" style="height:44em;  overflow: scroll;" >
+<h2 >Stock Status</h2>
+<select id="desc" name="desc" class="drop">
+      <option value="default">Select</option>
+      <option value="r_h">Reproductive Health</option>
+      <option value="malaria">Malaria</option>
+      <option value="all">All Commodities</option>
+			
+	</select>
+<div id=""></div>
+	</div>
+	
+	<div class="multiple_chart_content"  >
+<h2 >Ordering Rate</h2>
+<div id=""></div>
+	</div>
+	<div class="multiple_chart_content"  >
+<h2 >Notifications</h2>
+<div style="display: table-row;  ">
+    			<div style="display: table-cell;padding-bottom: 2em; ">
+      				<label style=" font-weight: ">Total No of Orders Placed </label>
+            			    				</div>
+    				<div style="display: table-cell;padding-bottom: 2em">
+      				<a class="link" href="#" >14</a>
     				</div>
   				</div>
   				
   				<div style="display: table-row; ">
-    			<div style="display: table-cell;padding-bottom: 3em">
-      				<label style="font-weight: 600">Total Value of Orders Placed </label>
-            		<a class="badge success" href="#" >1,221,001</a>
+    			<div style="display: table-cell;padding-bottom: 2em">
+      				<label style="font-weight: ">Total Value of Orders Placed </label>
+            		</div>
+    				<div style="display: table-cell;padding-bottom: 2em">
+      				<a class="link" href="#" >1,221,001</a>
     				</div>
   				</div>
   				
   				<div style="display: table-row;">
-    			<div style="display: table-cell; padding-bottom: 3em">
-      				<label style="font-weight: 600">Total District Drawing Rights Bal (2013/2014)</label>
-            		 <a class="badge success" href="#" >6,000,000</a>
+    			<div style="display: table-cell; padding-bottom: 2em">
+      				<label style="font-weight: ">Total District Drawing Rights Bal (2013/2014)</label>
+            		     				</div>
+    				<div style="display: table-cell;padding-bottom: 2em">
+      				<a class="link" href="#" >6,000,000</a>
     				</div>
   				</div>
   				
-  				<div style="display: table-row;">
-    			<div style="display: table-cell; padding-bottom: 3em">
-      				<label style="font-size: 1.6em;font-weight: 600">Total Cost of Expired Commodities </label>
-            		<a class="badge success" href="#" >457,100</a>
-    				</div>
-  				</div>
   				
-  				<div style="display: table-row;">
-    			<div style="display: table-cell;padding-bottom: 3em">
-      				<label style="font-size: 1.6em;font-weight: 600">Orders Lead Time</label>
-            		<a class="badge success" href="#" >12 days</a>
-    				</div>
-  				</div>
-			
+  				
+  				
 	</div>
 	
-</div>
-
-<div class="stockstatus">
-	<h2>Stock Status</h2>
 	
-	<div class="stockstatus_dropdown">
-	<select id="desc" name="desc" class="drop">
-    <option>Select Commodity Name</option>
-		<?php 
-		foreach ($drugs_array as $drugs) {
-			$id=$drugs->id;
-			$kemsa=$drugs->Kemsa_Code;
-			$drug=$drugs->Drug_Name;
-			?>
-			<option value="<?php echo $id; ?>"><?php echo $drug; ?></option>
-			
-		<?php } ?>		
-	</select>
-		<select id="facilities" class="drop">
-		<option>Select Facility</option>
-		<?php 
-		foreach ($facilities as $counties) {
-			$id=$counties->facility_code;
-			$county=$counties->facility_name;?>
-			<option value="<?php echo $id; ?>"><?php echo $county; ?></option>
-		<?php } ?>
-	</select>
-	
-</div>
-	<div class="stockstatuschart">
-	
-	
+	<div class="multiple_chart_content"   >
+<h2 >Cost of Orders</h2>
+<div id=""></div>
 </div>
 	
+	<div class="multiple_chart_content"  >
+<h2 >Allocated Drawing rights/Drawing rights Bal</h2>
+<div id=""></div>
 </div>
 
 
-<div class="cost_expiries">
-	<h2>Cost of Expiries</h2>
-	<div class="cost_expirieschart">
-	
-	
-</div>
-</div>
-
-<div class="cost_orders">
-	<h2>Cost of orders</h2>
-	<div class="cost_orderschart">
-	
-	
+	<div class="multiple_chart_content"  >
+<h2 >Cost of Expired commodities</h2>
+<div id=""></div>
 </div>
 	
-</div>
-
-<div class="consumption">
-	<h2>Consumption Trends</h2>
-	<div class="consumptionchart">
-	
-	
-</div>
-</div>
-
-<div class="lead_time">
-	<h2>Lead Time</h2>
-	<div class="lead_timechart">
-	
-	
-</div>
-</div>
+	<div class="multiple_chart_content"  >
+		<h2 >Cummulative Order Fill rate</h2>
+		<div id=""></div>
 
 </div>
+	
+	<div class="multiple_chart_content" style="height:60em">
+	<h2>Order Lead Time</h2>
+	<table width="100%" height="60" style="font-size:small">
+		<tr>
+		<td> Key</td>
+		<td><div class="success">Order - Approval</div></td>
+		<td><div style="background-color: #FFF6BF;"><font>Approval-Delivery</font></div></td>
+		<td><div style="background-color:#FF4545"><font>Delivery-Update</font></div></td>
+		<td><div style="background-color:#000000" ><font color="white">Turn Around Time	</font></div></td></tr>
+	
+		</table>
+
+	<div id="" ></div>
+	</div>
+  
+ 
+ </div> 
+ 
+
+  
+  
+            
+ 
+ 
